20f0c29809c03de3842fc3d93db22b2c35a913c8
Jonathan_PL <jlamoureux@stetson.edu>
2021-03-24 15:52:18 -0400

    Messing with hooks

--- src/components/settings.js
+++ src/components/settings.js
@@ -5,12 +5,14 @@ import faker from 'faker';
 
 export default function Settings() {
   const [startDate, setStartDate] = useState(new Date());
+  const [username, setUsername] = useState(faker.internet.userName());
+  const [email, setEmail] = useState(faker.internet.email());
   return (
     <div id='center'>
       <h1>User Settings</h1>
-      Username: <input type="text" value={faker.internet.userName()}/>
+      Username: <input type="text" value={username}/>
       <br/>
-      Email: <input type="text" value={faker.internet.email()}/>
+      Email: <input type="text" value={email}/>
       <br/>
       Birthdate: <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
       <br/>
