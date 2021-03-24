b083f637491f8838b08c957b84576fc90397a9d1
Jonathan_PL <jlamoureux@stetson.edu>
2021-03-24 14:54:07 -0400

    Updated the settings page and used faker

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import faker from 'faker';
 
 export default function Settings() {
   const [startDate, setStartDate] = useState(new Date());
   return (
-    <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+    <div id='center'>
+      <h1>User Settings</h1>
+      Username: <input type="text" value={faker.internet.userName()}/>
+      <br/>
+      Email: <input type="text" value={faker.internet.email()}/>
+      <br/>
+      Birthdate: <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+      <br/>
+      <button>Submit</button>
+    </div>
   );
 }
